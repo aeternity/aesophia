a1c9517ce66d9062a71bcc52ead12135b08979f5
Hans Svensson <hanssv@gmail.com>
2024-01-23 16:09:37 +0100

    [Ceres] Handle Bytes.to_any_size in calldata (#495)
    * [Ceres] Handle Bytes.to_any_size in calldata
    
    * Edit CHANGELOG

--- test/aeso_calldata_tests.erl
+++ test/aeso_calldata_tests.erl
@@ -85,6 +85,7 @@ compilable_contracts() ->
      {"funargs", "bitsum", ["Bits.clear(Bits.clear(Bits.all, 4), 2)"]}, %% Order matters for test
      {"funargs", "bitsum", ["Bits.set(Bits.set(Bits.none, 4), 2)"]},
      {"funargs", "read", ["{label = \"question 1\", result = 4}"]},
+     {"funargs", "any_bytes", ["Bytes.to_any_size(#0011AA)"]},
      {"funargs", "sjutton", ["#0011012003100011012003100011012003"]},
      {"funargs", "sextiosju", ["#01020304050607080910111213141516171819202122232425262728293031323334353637383940"
                                "414243444546474849505152535455565758596061626364656667"]},
